c6b0fadc821209c26a04bc145abcfed205f6e136
abbasmir12 <abbasmir12@gmail.com>
2025-10-23 16:10:28 +0500

    Vercel deploy update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  env: {
-    HUGGINGFACE_API_KEY: process.env.HUGGINGFACE_API_KEY,
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
